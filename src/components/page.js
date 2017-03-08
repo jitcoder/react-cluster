746b8a789392f27b2c3cfcff0ecd58f5cd7ca569
Sameer Mirza <sameer.mirza@fox.com>
2017-03-08 01:36:41 -0800

    blah

--- src/components/page.js
+++ src/components/page.js
@@ -11,11 +11,13 @@ export default class Page extends React.Component {
         <head>
           <title>{this.props.title}</title>
           <script>
-            window.__INITIAL_STATE__ = {JSON.stringify(this.props.initialState)}
+            window.__INITIAL_STATE__ = {JSON.stringify(this.props.initialState)};
           </script>
         </head>
         <body>
-          {this.props.children}
+          <div id={'app'}>
+            {this.props.children}
+          </div>
           <script src={'/bundle.js'}></script>
         </body>
       </html>
