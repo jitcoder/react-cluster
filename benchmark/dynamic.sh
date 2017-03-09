226befad8f0855fcd635bfeae834afc35c600857
Sameer Mirza <sameer.mirza@fox.com>
2017-03-08 16:27:50 -0800

    blah

---
+++ benchmark/dynamic.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ab -n512 -c128 http://localhost:3000/products/51234
--- src/master.js
+++ src/master.js
@@ -8,6 +8,9 @@ import jade from 'jade';
 import Product from './components/product';
 import * as path from 'path';
 
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
 const workers = workerFarm(require.resolve('./worker'));
 const template = jade.compileFile(path.resolve(__dirname, '../public/template.jade'));
 
@@ -19,8 +22,31 @@ app.get('/static/products/:id', (req, res) => {
   res.sendFile(path.resolve(__dirname, `../public/products/${req.params.id}.html`));
 });
 
+app.get('/', (req, res) => {
+  const products = require('../data/products.json');
+  const links = [];
+
+  for (let i = 0; i < products.length; i++){
+    links.push(`
+    <a href="/static/products/${products[i].id}">${products[i].displayName} (static)</a> &nbsp;
+    <a href="/products/${products[i].id}">${products[i].displayName} (dynamic)</a><br />
+    `);
+  }
+
+  res.send(`
+  <html>
+  <head>
+  <title>index</title>
+  </head>
+  <body>
+  ${links.join('')}
+  </body>
+  </html>
+  `)
+})
+
 app.get('/products/:id', (req, res) => {
-  const product = require('../data/products.json').filter(p => p.id === req.params.id);
+  const product = require('../data/products.json').find(p => p.id === parseInt(req.params.id));
   if (product) {
     const sales = require('../data/sales.json').filter(s => s.PRODUCT_ID === product.id);
 
@@ -29,8 +55,10 @@ app.get('/products/:id', (req, res) => {
       initialState: JSON.stringify({ product, sales }),
       initialRender: ReactDOMServer.renderToString(<Product product={product} sales={sales} />)
     }
+
+    res.send(template(state));
   } else {
-    req.send('uh oh');
+    res.send('uh oh');
   }
 })
 
